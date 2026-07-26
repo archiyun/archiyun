6200d9c03aea36c9839ec33dfe59de9d64272dfb
Arsenova <furina.see.fun@gmail.com>
2026-07-26 14:00:05 +0800

    feat: use GitHub avatar as blog avatar

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -13,7 +13,7 @@ function pluginPath(path: string) {
 	return pathToFileURL(resolve(`./remark-plugins/${path}.ts`)).href
 }
 
-const defaultOgImage = new URL('/arsenova.png', blogConfig.url).toString()
+const defaultOgImage = 'https://github.com/akiba-miku.png'
 
 // 此处配置无需修改
 export default defineNuxtConfig({
